8c8fec994fd29eb3a092008e12f0f92f9a41c82a
Kaung101 <khin.tun1@mail.kmutt.ac.th>
2026-03-15 22:16:27 +0700

    fix:dashboard route test

--- src/layouts/guest-layout.tsx
+++ src/layouts/guest-layout.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/components/footer/footer";
 export default function GuestLayout() {
   return (
     <div className="flex min-h-screen flex-col">
-      <Navbar isLoggedIn={true} />
+      <Navbar isLoggedIn={false} />
       {/*  Main content area */}
       <main className="flex-1">
         <Outlet />
