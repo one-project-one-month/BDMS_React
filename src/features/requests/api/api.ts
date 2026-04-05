0e0f0e563d3546c9fc1f159909a02570afb5f5e5
ZawHlaingPhyoTsuki <tro2233zhp@gmail.com>
2026-04-05 16:40:46 +0700

    feat: refactor hospital API calls and update blood request types

--- src/features/requests/api/api.ts
+++ src/features/requests/api/api.ts
@@ -1,8 +1,11 @@
 import api from "@/api/axios-client";
 import type { ApiResponse, Hospital } from "../requests.types";
+import { HOSPITAL_ENDPOINTS } from "@/api/endpoints/hospital.endpoint";
 
 export const getHospitals = async (): Promise<Hospital[]> => {
-  const { data } = await api.get<ApiResponse<Hospital[]>>("/Hospital/list");
+  const { data } = await api.get<ApiResponse<Hospital[]>>(
+    HOSPITAL_ENDPOINTS.LIST,
+  );
 
   if (!data.isSuccess || data.isError) {
     throw new Error(data.message || "Failed to fetch hospitals");
--- src/features/requests/requests.types.ts
+++ src/features/requests/requests.types.ts
@@ -1,40 +1,75 @@
+export type BloodRequestUrgency = "low" | "medium" | "high" | "critical";
+
+export type BloodGroup =
+  | "A+"
+  | "A-"
+  | "B+"
+  | "B-"
+  | "AB+"
+  | "AB-"
+  | "O+"
+  | "O-";
+
+export type BloodRequestStatus =
+  | "pending"
+  | "cancelled"
+  | "approved"
+  | "rejected"
+  | "fulfilled";
+
+export type BloodRequestStatusa =
+  | "pending"
+  | "cancelled"
+  | "approved"
+  | "rejected"
+  | "fulfilled";
+
 export interface BloodRequest {
   id: number;
+  userId: number;
+  hospitalId: number;
+  bloodRequestCode: string;
   patientName: string;
-  bloodType: string;
-  hospitalName: string;
-  address: string;
-  numberOfUnits: number;
+  bloodGroup: BloodGroup;
+  unitsRequired: number;
+  contactPhone: string;
+  urgency: BloodRequestUrgency;
   requiredDate: Date;
-  requestType: "emergency" | "pre-booked";
-  relationshipToPatient: string;
-  contactNumber: string;
-  reasonForRequest: string;
-  additionalNotes?: string;
-  status: "pending" | "approved" | "rejected" | "fulfilled";
+  status: BloodRequestStatus;
+  reason: string;
+  approvedBy: number | null;
+  approvedAt: Date | null;
   createdAt: Date;
   updatedAt: Date;
+  deletedAt: Date | null;
 }
 
 export interface CreateBloodRequestPayload {
+  userId: number;
+  hospitalId: number;
   patientName: string;
-  bloodType: string;
-  hospitalName: string;
-  address: string;
-  numberOfUnits: number;
-  requiredDate: Date;
-  requestType: "emergency" | "pre-booked";
-  relationshipToPatient: string;
-  contactNumber: string;
-  reasonForRequest: string;
-  additionalNotes?: string;
+  bloodGroup: string;
+  unitsRequired: number;
+  contactPhone: string;
+  urgency: string;
+  requiredDate: {
+    year: number;
+    month: number;
+    day: number;
+    dayOfWeek: number;
+  };
+  reason: string;
+}
+
+export interface UpdateBloodRequestPayload extends CreateBloodRequestPayload {
+  id: number;
 }
 
 export interface ApiResponse<T> {
-    isSuccess: boolean;
-    isError: boolean;
-    data: T;
-    message: string;
+  isSuccess: boolean;
+  isError: boolean;
+  data: T;
+  message: string;
 }
 
 export interface Hospital {
