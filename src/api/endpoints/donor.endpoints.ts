9d9e488dd819aba693d6f74ccf82ce75af215905
NwayOoPyaeZo <nwayoopyaezo@gmail.com>
2026-04-10 08:56:17 +0700

    chore:align-donor-endpoints

--- src/api/endpoints/donor.endpoints.ts
+++ src/api/endpoints/donor.endpoints.ts
@@ -1,7 +1,8 @@
 export const DONOR_ENDPOINTS = {
-	LIST: "/Donor/list",
-	CREATE: "/Donor/create",
-	GET_BY_ID: (id: string | number) => `/Donor/${id}`,
-	UPDATE: "/Donor/update",
-	DELETE: "/Donor/delete",
+  LIST: "/Donor/list",
+  CREATE: "/Donor/create",
+  UPDATE: "/Donor/update",
+  DELETE: (id: string | number) => `/Donor/delete?donorId=${id}`,
+  GET_BY_ID: (id: string | number) => `/Donor/${id}?donorId=${id}`,
+  UPDATE_STATUS: (id: string | number) => `/Donor/${id}/status`,
 } as const;
